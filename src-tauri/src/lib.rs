86208163f755f440a1e0022d8e7db6aa8b9408f5
sukehama <sukmanahmed.09@gmail.com>
2026-07-14 17:58:57 +0200

    nadam se da je zadnji

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,6 +2,7 @@
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init()) // <--- DODAJ OVU LINIJU ZA RESTART!
         .setup(|app| {
             if cfg!(debug_assertions) {
                 app.handle().plugin(
